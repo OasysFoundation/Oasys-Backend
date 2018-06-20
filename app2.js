fcc7a15a8ca2af09cfbfb7fd228f8f2cd1e8c817
Robbie Zuazua <rzuazua@utexas.edu>
2018-06-19 20:20:46 -0500

    updated working backend

---
+++ app2.js
@@ -0,0 +1,249 @@
+#!/usr/bin/env nodejs
+
+/****************************
+Starting Point of Application
+****************************/
+const express = require('express')
+const util = require('util');
+const bodyParser = require('body-parser')
+const mongo = require('./mongo.js');
+const app = express()
+
+
+//Middleware for CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*,Content-Type,id");
+    next();
+});
+
+// Middleware to parse JSON 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//TODO: Setup https
+app.get('/', (req, res) => {
+  res.send('Documentation: https://docs.google.com/document/d/1aRe4420DifJNUmK-BPdQocBaC6b8LkowPVv4TJN0jJQ/edit?usp=sharing');
+});
+
+/*
+Loads picture, title, description, tags, and url from "contents" db with published flag
+*/
+app.get('/GetContentsPreview', function (req, res) {
+  mongo.readPreviewFromMongo(function(result, err) { 
+    if (err){
+      console.log(err);
+      res.end("Unexpected Error from Db");
+    }
+    else {
+      trueResult = [];
+      for(i = 0; i < result.length; i++){
+        trueResult.push({
+           "picture" :      result[i].picture,
+           "title" :        result[i].title,
+           "description" :  result[i].description,
+           "tags" :         result[i].tags,
+           "url" :          result[i].url
+        });
+      }
+      res.json(trueResult); 
+    }
+  });
+});
+
+/*
+Loads full JSON of selected experience from “contents” db
+*/
+
+app.get('/:userId/:contentId', function (req, res) {
+
+  userId = req.params.userId;
+  contentId = req.params.contentId;
+
+  mongo.readContentFromMongo(userId, contentId, function(result,err) { 
+    if (err){
+      console.log(err);
+      res.end("Unexpected Error from Db");
+    }
+    else{
+      res.json(result); 
+    }
+  });
+
+});
+
+/*
+Loads avg rating of content from "ratings" db
+*/
+
+app.get('/:userId/:contentId/avgRating', function (req, res) {
+
+  contentId = req.params.contentId;
+
+  mongo.readRatingFromMongo(contentId, function(result,err) { 
+      if (err){
+        console.log(err);
+        res.end("Unexpected Error from Db");
+      }
+      else {
+        avg = 0;
+        sum = 0;
+        count = 0;
+
+        for (var i = 0; i < result.length; i++) {
+          sum += result[i].rating;
+          count +=1;
+        }
+
+        if (count != 0) {
+          avg = sum/count;
+        }
+        res.json(avg); 
+      }
+  });
+
+});
+
+/*
+Write rating for content into "ratings" db
+*/
+
+app.post('/:userId/:contentId/rate/:rating', function (req, res) {
+
+  userId = req.params.userId;
+  contentId = req.params.contentId;
+  rating = req.params.rating;
+
+  mongo.WriteRatingToMongo(userId, contentId, rating, function(result,err) { 
+    if (err){
+      console.log(err);
+      res.end("Unexpected Error from Db");
+    }
+    else{
+      res.json(result); 
+    }
+  });
+
+});
+
+/*
+Write data into to “contents” db
+*/
+
+app.post('/:userId/:contentId/save', function (req, res) {
+
+  userId = req.params.userId;
+  contentId = req.params.contentId;
+
+   if(!req.body){
+      res.end("Error: Request body is empty.");
+    }
+    else if(req.body.published==1){
+      if(!req.body.picture || !req.body.title || !req.body.description || !req.body.tags || !req.body.url)
+        res.end("You cannot publish unless you provide the picture url, title, description, tags, and url");
+      else {
+        jsonBody =req.body;
+        mongo.writeContentToMongo("publish", jsonBody, userId, contentId, function(result,err) { 
+        if (err){
+          console.log(err);
+          res.end("Unexpected Error from Db");
+        }
+        else{
+          console.log(result);
+          res.send(result); 
+        }
+      });
+      }
+
+    }
+    else{
+      jsonBody =req.body;
+      mongo.writeContentToMongo("save", jsonBody, userId, contentId, function(result,err) { 
+        if (err){
+          console.log(err);
+          res.end("Unexpected Error from Db");
+        }
+        else{
+          console.log(result);
+          res.send(result); 
+        }
+      });
+    }
+
+});
+
+
+//Save editor JSON to DB
+//Saves to 'graph' db in mongo
+
+app.post('/saveEditor', (req, res) => {
+ 
+ if(!req.body){
+    res.end("Please provide something in the body. For more information: https://docs.google.com/document/d/1aRe4420DifJNUmK-BPdQocBaC6b8LkowPVv4TJN0jJQ/edit");
+  }
+  else{
+    jsonBody =req.body;
+    mongo.writeToMongo(jsonBody, function(result,err) { 
+      if (err){
+        console.log(err);
+        res.end("Unexpected Error from Db");
+      }
+      else if (!result.insertedId){
+        res.end("Unexpected Error from Db - no inserted ID");
+      }
+      else{
+        console.log(result);
+        console.log(result.insertedId);
+        res.send(result.insertedId); 
+      }
+    });
+  }
+});
+
+//Save editor JSON to DB
+app.post('/updateEditor', (req, res) => {
+  if(!req.headers.id){
+    res.end("Please provide the contentID in the header. For more information: https://docs.google.com/document/d/1aRe4420DifJNUmK-BPdQocBaC6b8LkowPVv4TJN0jJQ/edit");
+  }
+  else if(!req.body){
+    res.end("Please provide something in the body. For more information: https://docs.google.com/document/d/1aRe4420DifJNUmK-BPdQocBaC6b8LkowPVv4TJN0jJQ/edit");
+  } 
+  else {
+    jsonBody =req.body;
+    mongoId = req.headers.id;
+    mongo.updateMongo(jsonBody, mongoId, function(result,err) { 
+      if (err){
+        console.log(err);
+        res.end("Unexpected Error from Db");
+      }
+      else {
+        console.log(result);
+        res.send(result);
+      } 
+    });
+  }
+});
+
+//Delete editor JSON from DB
+app.get('/deleteEditor', (req, res) => {
+  if(!req.headers.id){
+    res.end("Please provide the contentID in the header. For more information: https://docs.google.com/document/d/1aRe4420DifJNUmK-BPdQocBaC6b8LkowPVv4TJN0jJQ/edit");
+  }
+  else {
+    mongoId = req.headers.id;
+    mongo.deleteFromMongo(mongoId, function(result,err) { 
+      if (err){
+        console.log(err);
+        res.end("Unexpected Error from Db");
+      }
+      else {
+        console.log(result);
+        res.send(result); 
+      }
+    });
+  }
+});
+
+
+app.listen(8080, () => console.log('Listening on port 8080'))
+
---
+++ mongo2.js
@@ -0,0 +1,209 @@
+
+/***************************************************************
+Module for Connecting, Reading, Writing, and Deleting from Mongo
+***************************************************************/
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const f = require('util').format;
+const assert = require('assert');
+const user = encodeURIComponent(process.env.USER_ID);
+const password = encodeURIComponent(process.env.PASSWORD);
+const authMechanism = 'DEFAULT';
+const url = f('mongodb://%s:%s@localhost:27017/oasysTest?authMechanism=%s',
+  user, password, authMechanism);
+
+var exports = module.exports = {};
+
+/***************************
+Reading from Mongo (helper functions)
+***************************/
+
+// Returns picture, title, description, tags, and url from "contents" db with published flag
+const getPreview = function(db, callback) {
+  const collection = db.collection('contents');
+  collection.find({'published': 1}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log("db response: ")
+      console.log(result)
+      callback(result);
+    });
+}
+
+// Returns full JSON of specified user id and content id
+const findContent = function(userId, contentId, db, callback) {
+  const collection = db.collection('contents');
+  collection.find({'userId': userId , 'contentId': contentId}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log("db response: ")
+      console.log(result)
+      callback(result);
+    });
+}
+
+
+// Returns all ratings for given content Id
+const findRating = function(db, contentId, callback) {
+  const collection = db.collection('ratings');
+  collection.find({'contentId': contentId}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result)
+    callback(result);
+  });
+}
+
+// writes rating to ratings db
+const writeRating = function(db, contentId, rating, callback) {
+  const collection = db.collection('ratings');
+  rating = parseInt(rating);
+  collection.insertOne({"contentId": contentId, "rating": rating}, function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    callback(result);
+  });  
+}
+
+// saves content to "contents" db
+const saveContent = function(db, userId, contentId, data, callback) {
+  console.log(data);
+  newData = data.data;
+  const collection = db.collection('contents');
+  collection.update({"contentId": contentId}, { $set: { "data" : newData } }, {"upsert": true}, function(err, result) {
+    if (err) throw err;
+    else{
+      console.log("Update successful");
+      callback(result);
+    }
+  });  
+}
+
+// saves content including title,picture,url etc. and publishes content so preview mode can grab it
+const publishContent = function(db, userId, contentId, data, callback) {
+  console.log(data);
+  newData = data.data;
+  title = data.title;
+  picture = data.picture;
+  published = data.published;
+  description = data.description;
+  tags = data.tags;
+  my_url = data.url;
+
+  const collection = db.collection('contents');
+  collection.update({"contentId": contentId}, { $set: { "data" : newData, "title": title, "picture": picture, "description" : description, "published" : published, "tags":tags,"url":my_url} }, {"upsert": true}, function(err, result) {
+    if (err) throw err;
+    else{
+      console.log("Update successful");
+      callback(result);
+    }
+  });  
+}
+
+
+// /***************************
+// Deleting from Mongo (helpers)
+// ****************************/
+// const removeDocument = function(db, mongoId, callback) {
+//   const collection = db.collection('graph');
+//   collection.deleteOne({"_id": ObjectId(mongoId)}, function(err, result) {
+//     if (err) throw err;
+//     assert.equal(err, null);
+//     callback(result);
+//   });    
+// }
+
+
+// Write to "contents" db
+exports.writeContentToMongo = function(status, data, userId, contentId, callback) {
+  if (status == "save"){
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      else {
+        console.log("Connected successfully to server");
+        saveContent(db, userId, contentId, data, function(result,err) {
+          if (err) throw err;
+           db.close();
+           callback(result);
+          });
+      }
+    });
+  }
+  else{
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      else {
+        console.log("Connected successfully to server");
+        publishContent(db, userId, contentId, data, function(result,err) {
+          if (err) throw err;
+           db.close();
+           callback(result);
+          });
+      }
+    });
+
+  }
+};
+
+// Write to "ratings" db
+exports.WriteRatingToMongo = function(userId, contentId, rating, callback) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    else {
+      console.log("Connected successfully to server");
+      writeRating(db, contentId, rating, function(result,err) {
+        if (err) throw err;
+        db.close();
+        callback(result);
+        });
+    }
+  });
+};
+
+
+// Reads from "contents" db
+exports.readPreviewFromMongo = function(callback) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    else {   
+      console.log("Connected successfully to db");
+      getPreview(db, function(result,err) {
+        if (err) throw err;
+        db.close();
+        callback(result);
+        });
+    }
+  });
+};
+
+// Reads from "contents" db
+exports.readContentFromMongo = function(userId, contentId, callback) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    else {   
+      console.log("Connected successfully to db");
+      findContent(userId, contentId, db, function(result,err) {
+        if (err) throw err;
+        db.close();
+        callback(result);
+        });
+    }
+  });
+};
+
+
+//Reads from "ratings" db
+exports.readRatingFromMongo = function(contentId, callback) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    else{
+      console.log("Connected successfully to server");
+      findRating(db, contentId, function(result,err) {
+        if (err) throw err;
+        db.close();
+        callback(result);
+        });
+    }
+  });
+};
+
+
+
+
